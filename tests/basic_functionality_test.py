e9eae2e663a3cc717e7c96d66bc6aaa84fb73c30
Samuel Yvon <samuelyvon9@gmail.com>
2023-02-23 15:59:11 -0500

    Quick bail?

--- tests/basic_functionality_test.py
+++ tests/basic_functionality_test.py
@@ -14,4 +14,7 @@ def test_has_ipv4_or_ipv6() -> None:
         has_any_ip |= netifaces.AF_INET in address_table
         has_any_ip |= netifaces.AF_INET6 in address_table
 
+        if has_any_ip:
+            break
+
     assert has_any_ip, "Test failure; no AF_INET address of any kind found"
