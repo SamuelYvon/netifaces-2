de725caa56c6befd55e2681243405b1c9b7c92c0
Samuel Yvon <samuelyvon9@gmail.com>
2023-02-23 15:57:19 -0500

    Check for ipv4/v6

--- tests/basic_functionality_test.py
+++ tests/basic_functionality_test.py
@@ -3,3 +3,15 @@ import netifaces
 
 def test_interfaces() -> None:
     assert len(netifaces.interfaces())
+
+
+def test_has_ipv4_or_ipv6() -> None:
+    has_any_ip = False
+
+    for interface in netifaces.interfaces():
+        address_table = netifaces.ifaddresses(interface)
+
+        has_any_ip |= netifaces.AF_INET in address_table
+        has_any_ip |= netifaces.AF_INET6 in address_table
+
+    assert has_any_ip, "Test failure; no AF_INET address of any kind found"
--- tests/test.py
+++
@@ -1,4 +0,0 @@
-import netifaces
-
-if __name__ == "__main__":
-    assert len(netifaces.interfaces())
