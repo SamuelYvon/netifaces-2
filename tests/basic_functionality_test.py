21efd693379b2fbbf26ac526282dc3fc3307fb5b
Samuel Yvon <samuelyvon9@gmail.com>
2023-02-28 13:13:20 -0500

    Add link layer

--- tests/basic_functionality_test.py
+++ tests/basic_functionality_test.py
@@ -18,3 +18,17 @@ def test_has_ipv4_or_ipv6() -> None:
             break
 
     assert has_any_ip, "Test failure; no AF_INET address of any kind found"
+
+
+def test_has_link_layer():
+    has_any_link = False
+
+    for interface in netifaces.interfaces():
+        address_table = netifaces.ifaddresses(interface)
+
+        has_any_link |= netifaces.AF_PACKET in address_table
+
+        if has_any_link:
+            break
+
+    assert has_any_link, "Test failure; no AF_PACKET address of any kind found"
